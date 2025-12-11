699c31716318254422ef4fba718fc8c7f4a87d3d
NATO-dotcom <nato114267@students.must.ac.ke>
2025-12-11 03:01:43 +0300

    Modified my creation of tables code

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -38,10 +38,10 @@ CREATE TABLE ORDERS (
 
 -- Create Order Details table
 CREATE TABLE ORDER_DETAILS (
-    orderdetailid INT PRIMARY KEY,
-    order_id INT,
-    book_id INT,
-    quantity DOUBLE,
-    FOREIGN KEY (order_id) REFERENCES ORDERS(order_id),
-    FOREIGN KEY (book_id) REFERENCES BOOKS(book_id)
+    orderdetail_id INT NOT NULL PRIMARY KEY,
+    order_id INT NOT NULL,
+    book_id INT NOT NULL,
+    quantity DOUBLE NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
