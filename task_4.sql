60b141db1a0d5990a434ba92ab07be1ca8a21eb8
NATO-dotcom <nato114267@students.must.ac.ke>
2025-12-11 03:40:48 +0300

    Created task_4.sql file modified waah

--- task_4.sql
+++ task_4.sql
@@ -1,11 +1,3 @@
 -- Prints the full description of the 'Books' table without using DESCRIBE or EXPLAIN.
-SELECT 
-    COLUMN_NAME,
-    COLUMN_TYPE,
-    IS_NULLABLE,
-    COLUMN_DEFAULT,
-    COLUMN_KEY,
-    EXTRA
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'Books'
-ORDER BY ORDINAL_POSITION;
+SHOW FULL COLUMNS FROM alx_book_store.Books;
+
