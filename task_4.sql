0d3d133bd24fc0d55aaa09b64f2303354fe22120
NATO-dotcom <nato114267@students.must.ac.ke>
2025-12-11 03:39:19 +0300

    Created task_4.sql file modified YOOH

--- task_4.sql
+++ task_4.sql
@@ -1,3 +1,11 @@
 -- Prints the full description of the 'Books' table without using DESCRIBE or EXPLAIN.
-USE alx_book_store;
-SHOW CREATE TABLE Books;
+SELECT 
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_DEFAULT,
+    COLUMN_KEY,
+    EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'Books'
+ORDER BY ORDINAL_POSITION;
